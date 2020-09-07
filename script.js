3d8f2c34d403e5ad54be9df006e9d42e6d778308
Anne Culpepper <anne.culpepper@gmail.com>
2020-09-07 16:44:44 -0500

    used textContent to set the text of HTML element to the data from API, limiting the description by using substring() method on the p element...keeping each card equal length.

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// fetch('https://ghibliapi.herokuapp.com/films')
-//   .then((response) => {
-//     return (data = response.json());
-//   })
-//   .then((data) => {
-//     console.log(data);
-//     for (let film of data) {
-//       console.log(film.title);
-//     }
-//   })
-
-//   .catch((err) => {
-//     console.log(err);
-//   });
-
 const app = document.getElementById('root');
 console.log(app);
 const logo = document.createElement('img');
@@ -23,3 +8,32 @@ container.setAttribute('class', 'container');
 
 app.appendChild(logo);
 app.appendChild(container);
+
+fetch('https://ghibliapi.herokuapp.com/films')
+  .then((response) => {
+    return (data = response.json());
+  })
+  .then((data) => {
+    // console.log(data);
+    for (let film of data) {
+      const card = document.createElement('div');
+      card.setAttribute('class', 'card');
+
+      const h1 = document.createElement('h1');
+      h1.textContent = film.title;
+
+      const p = document.createElement('p');
+      film.description = film.description.substring(0, 300);
+      p.textContent = `${film.description}...`;
+
+      container.appendChild(card);
+      card.appendChild(h1);
+      card.appendChild(p);
+      console.log(film.title);
+      console.log(film.description);
+    }
+  })
+
+  .catch((err) => {
+    console.log(err);
+  });
