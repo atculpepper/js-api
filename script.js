9aed256374a37b5555ec42e2e3e19015839aa657
Anne Culpepper <anne.culpepper@gmail.com>
2020-09-07 16:17:40 -0500

    wrote a for loop within the .then of a fetch method that prints each film title to the console

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 fetch('https://ghibliapi.herokuapp.com/films')
   .then((response) => {
-    return response.json();
+    return (data = response.json());
   })
   .then((data) => {
     console.log(data);
-    // let data = JSON.parse(data);
+    for (let film of data) {
+      console.log(film.title);
+    }
   })
+
+  //  (let film of data) {
+  //   console.log(film.title);
+  // }
+
   .catch((err) => {
     console.log(err);
   });
+
+// let films = JSON.parse(films);
+
+// data.forEach((movie) => {
+//   console.log(movie.title);
+// });
