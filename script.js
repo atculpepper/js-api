5abdc51f092f419903686055db4dfc5d3a5f8b50
Anne Culpepper <anne.culpepper@gmail.com>
2020-09-07 16:37:14 -0500

    brought in logo as static asset and used .getElementById and creatElement methods to insert some elements to HTML using JS

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-fetch('https://ghibliapi.herokuapp.com/films')
-  .then((response) => {
-    return (data = response.json());
-  })
-  .then((data) => {
-    console.log(data);
-    for (let film of data) {
-      console.log(film.title);
-    }
-  })
+// fetch('https://ghibliapi.herokuapp.com/films')
+//   .then((response) => {
+//     return (data = response.json());
+//   })
+//   .then((data) => {
+//     console.log(data);
+//     for (let film of data) {
+//       console.log(film.title);
+//     }
+//   })
 
-  //  (let film of data) {
-  //   console.log(film.title);
-  // }
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
-  .catch((err) => {
-    console.log(err);
-  });
+const app = document.getElementById('root');
+console.log(app);
+const logo = document.createElement('img');
+logo.src = 'ghibli_logo.png';
 
-// let films = JSON.parse(films);
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
 
-// data.forEach((movie) => {
-//   console.log(movie.title);
-// });
+app.appendChild(logo);
+app.appendChild(container);
